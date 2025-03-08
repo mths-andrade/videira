89e1f297dadc4e87cf8d6fccb41de13445a4fef2
mths-andrade <159069202+mths-andrade@users.noreply.github.com>
2025-03-08 12:55:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ import plotly.express as px
 
 @st.cache_resource
 def carrega_modelo():
-    # https://drive.google.com/file/d/1VPpwL8cagLfP8j6smglsCrarV0182Jkh/view?usp=drive_link
-    url = 'https://drive.google.com/uc?id=1VPpwL8cagLfP8j6smglsCrarV0182Jkh'
+    # https://drive.google.com/file/d/1VRVIn089G5dbSVPSIkpQWEYJfhTZYm34/view?usp=drive_link
+    url = 'https://drive.google.com/uc?id=1VRVIn089G5dbSVPSIkpQWEYJfhTZYm34'
     gdown.download(url, 'modelo_quantizado16bits.tflite')
     interpreter = tf.lite.Interpreter(model_path='modelo_quantizado16bits.tflite')
     interpreter.allocate_tensors()
